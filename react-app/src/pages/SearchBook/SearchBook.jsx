b760f74bea05433f17a5eb57b66b64362eef8e5c
Veronika Bahniuk <nika6@DESKTOP-B6S6DIR>
2025-01-23 13:44:51 +0200

    Edit code

--- react-app/src/pages/SearchBook/SearchBook.jsx
+++ react-app/src/pages/SearchBook/SearchBook.jsx
@@ -10,16 +10,9 @@ import './SearchBook.scss';
 function SearchBook() {
     // url серверної частини
     const apiUrl = import.meta.env.VITE_URL;
-    // параметри пошуку
-    const [author, setAuthor] = useState('');
-    const [title, setTitle] = useState('');
-    const [category, setCategory] = useState('');
-    const [language, setLanguage] = useState('');
-    const [format, setFormat] = useState('');
-    const [applyYears, setApplyYears] = useState(false);
-    const [startYear, setStartYear] = useState('');
-    const [endYear, setEndYear] = useState('');
-    // діапазон років видань 
+    const navigate = useNavigate();
+
+    // діапазон років видань (мінімальне та максимальне можливе значення)
     const [rangeYears, setRangeYears] = useState({ minYear: 0, maxYear: 0 });
     useEffect(() => {
         axios.get(apiUrl + '/rangeyears')
@@ -27,42 +20,37 @@ function SearchBook() {
             .catch(err => console.log(err))
     }, [])
 
-    const handleCheckboxChange = (event) => {
-        setApplyYears(event.target.checked);
-    };
+    // параметри пошуку
+    const [data, setData] = useState({
+        author: "", title: "", category: "", language: "",
+        format: "", applyYears: false, startYear: 0, endYear: 0
+    })
 
-    const navigate = useNavigate();
+    // зміна параметрів пошуку
+    const handleChange = (e) => {
+        if (e.target.name === 'applyYears')
+            setData(prev => ({ ...prev, [e.target.name]: e.target.checked }))
+        else
+            setData(prev => ({ ...prev, [e.target.name]: e.target.value }))
+    }
 
-    // функція відправки даних форми пошуку
-    // на сервер
+    // функція відправки даних форми пошуку на сервер
     const handleSubmit = (e) => {
         e.preventDefault()
         // параметри URL
         const params = new URLSearchParams();
-        if (title) {
-            params.append('title', title);
-        }
+        Object.entries(data).forEach(([key, value]) => {
+            if (value) {
+                if (key !== 'startYear' && key !== 'endYear' && key !== 'applyYears') {
+                    params.append(key, value);
+                }
+            }
+        });
 
-        if (author) {
-            params.append('author', author);
-        }
-
-        if (category) {
-            params.append('category', category);
-        }
-
-        if (language) {
-            params.append('language', language);
-        }
-
-        if (format) {
-            params.append('format', format);
-        }
-
-        if (applyYears) {
-            if (startYear && endYear) {
-                params.append('startYear', startYear);
-                params.append('endYear', endYear);
+        if (data.applyYears) {
+            if (data.startYear && data.endYear) {
+                params.append('startYear', data.startYear);
+                params.append('endYear', data.endYear);
             }
         }
 
@@ -75,75 +63,71 @@ function SearchBook() {
         <div className="container mt-3" style={{ width: "40%" }}>
             <form className="form" onSubmit={handleSubmit}>
                 <legend>Розширений пошук книг</legend>
-                <div className="form-group row"> {/** поля назви та автора */}
+                <div className="form-group row">
                     <label htmlFor="inputTitleBook">Назва книги</label>
                     <div className="col-sm-8">
                         <input type="text" className='form-control' id="inputTitleBook"
-                            placeholder='title'
-                            maxLength="200"
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}
+                            name='title' placeholder='title' maxLength="200"
+                            value={data.title}
+                            onChange={(e) => handleChange(e)}
                         />
                     </div>
                     <label htmlFor="inputAuthorBook">Автор</label>
                     <div className="col-sm-8">
-                        <input type="text" className="form-control" id="inputAuthorBook"
-                            placeholder="author"
-                            maxLength="100"
-                            value={author}
-                            onChange={(e) => setAuthor(e.target.value)}
+                        <input type="text" className="form-control" id="inputAuthorBook" name='author'
+                            placeholder="author" maxLength="100"
+                            value={data.author}
+                            onChange={(e) => handleChange(e)}
                         />
                     </div>
                 </div>
-                <div className="mb-3 col-sm-8">{/** поля категорії, мови та формату */}
+                <div className="mb-3 col-sm-8">
                     <label htmlFor="categorySelect" className="form-label">Категорія</label>
-                    <select id="categorySelect" className="form-select"
-                        onChange={(e) => setCategory(e.target.value)}>
+                    <select id="categorySelect" className="form-select" name='category'
+                        onChange={(e) => handleChange(e)}>
                         <option value=''>Будь-яка</option>
                         <option value='Художня література'>Художня література</option>
                         <option value='Нехудожня література'>Нехудожня література</option>
                     </select>
-
                     <label htmlFor="languageSelect" className="form-label mt-1">Мова</label>
-                    <select id="languageSelect" className="form-select"
-                        onChange={(e) => setLanguage(e.target.value)}>
+                    <select id="languageSelect" className="form-select" name='language'
+                        onChange={(e) => handleChange(e)}>
                         <option value=''>Будь-яка</option>
                         <option value='Українська'>Українська</option>
                         <option value='English'>English</option>
                     </select>
-
                     <label htmlFor="formatSelect" className="form-label mt-1">Формат</label>
-                    <select id="formatSelect" className="form-select"
-                        onChange={(e) => setFormat(e.target.value)}>
+                    <select id="formatSelect" className="form-select" name='format'
+                        onChange={(e) => handleChange(e)}>
                         <option value=''>Будь-який</option>
                         <option value='EPUB'>EPUB</option>
                         <option value='FB2'>FB2</option>
                     </select>
                 </div>
-                <div className='row g-2'> {/** поля діапазону років видання */}
+                <div className='row g-2'>
                     <div className="col-auto">
                         <label htmlFor="inputStartYear" className="form-label">з (початок)</label>
                         <input type="number" id="inputStartYear" className="form-control"
-                            step='1' min={rangeYears.minYear} max={rangeYears.maxYear}
-                            onChange={(e) => setStartYear(e.target.value)}
+                            name='startYear' step='1' min={rangeYears.minYear} max={rangeYears.maxYear}
+                            onChange={(e) => handleChange(e)}
                         />
                     </div>
                     <div className="col-auto">
                         <label htmlFor="inputEndYear" className="form-label">по (кінець)</label>
                         <input type="number" id="inputEndYear" className="form-control"
-                            step='1' min={rangeYears.minYear + 1} max={rangeYears.maxYear}
-                            onChange={(e) => setEndYear(e.target.value)}
+                            name='endYear' step='1' min={rangeYears.minYear + 1} max={rangeYears.maxYear}
+                            onChange={(e) => handleChange(e)}
                         />
                     </div>
                 </div>
-                <div className="form-check ms-1 mb-2"> {/** позначка застосування діапазону */}
+                <div className="form-check ms-1 mb-2">
                     <label className="form-check-label" htmlFor="inputApplyYear">
                         Застосувати діапазон років
                     </label>
                     <input className="form-check-input" type="checkbox"
-                        id="inputApplyYear"
-                        checked={applyYears}
-                        onChange={handleCheckboxChange}
+                        name='applyYears' id="inputApplyYear"
+                        checked={data.applyYears}
+                        onChange={(e) => handleChange(e)}
                     />
                 </div>
                 <button type="submit" className="btn btn-primary col-sm-6 mt-2">Шукати</button>
